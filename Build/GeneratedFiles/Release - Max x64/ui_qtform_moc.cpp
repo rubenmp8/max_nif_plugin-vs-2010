c8c8a13b522a48be945adcd9846903b48048167e
nexustheru <sehindel_19@hotmail.com>
2012-11-14 01:09:44 +0100

    added copy macro to auto add moc file to dir when building or u get link errors

--- Build/GeneratedFiles/Release - Max x64/ui_qtform_moc.cpp
+++ Build/GeneratedFiles/Release - Max x64/ui_qtform_moc.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'ui_qtform.h'
 **
-** Created: Wed 14. Nov 00:31:29 2012
+** Created: Wed 14. Nov 01:06:24 2012
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.3)
 **
 ** WARNING! All changes made in this file will be lost!
